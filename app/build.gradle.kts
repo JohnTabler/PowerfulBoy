885c29888166ad7158935fdac4aa7e2fbacc1078
JohnTabler <131303699+JohnTabler@users.noreply.github.com>
2026-06-03 16:19:37 -0700

    buildfix1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.powerfulboy.app"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.powerfulboy.app"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
